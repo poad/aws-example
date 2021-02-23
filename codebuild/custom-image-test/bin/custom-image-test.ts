9c7bc8fb000bcf0d62118e1581fdf0ea0754bec3
Kenji Saito <ken-yo@mbr.nifty.com>
2021-02-23 13:20:23 +0900

    Added the CodeBuild Custom Image test

---
+++ codebuild/custom-image-test/bin/custom-image-test.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { CustomImageTestStack } from '../lib/custom-image-test-stack';
+
+const app = new cdk.App();
+
+const env = app.node.tryGetContext('env') as string;
+const owner = app.node.tryGetContext('owner') as string;
+const repo = app.node.tryGetContext('repo') as string;
+const buildspec = app.node.tryGetContext('buildspec') as string;
+const image = app.node.tryGetContext('image') as string;
+
+new CustomImageTestStack(app, 'CustomImageTestStack', {
+    owner,
+    repo,
+    buildspec,
+    environment: env,
+    image,
+});

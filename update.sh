76c7c4fe8ff12c10e3c698fda04c30a519046659
Kenji Saito <ken-yo@mbr.nifty.com>
2021-03-31 06:59:20 +0900

    Add the update workflow

---
+++ update.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+
+CUR=$(pwd)
+
+YARN_PROJECTS="\
+  codebuild/custom-image-test\
+  lambda/cognito-console\
+  lambda/cognito-lambda\
+  lambda/cognito-lambda/lambda\
+  lambda/docker-lambda\
+  lambda/docker-lambda/lambda\
+  lambda/python
+"
+
+which jq >> /dev/null
+if [ $? -ne 0 ]
+then
+  echo "not found jq command"
+  exit 1
+fi
+
+# parse package.json
+dev_modules=$(echo -n $(cat package.json | jq -r ".devDependencies | to_entries | .[].key"))
+echo ${dev_modules}
+
+modules=$(echo -n $(cat package.json | jq -r ".dependencies | to_entries | .[].key"))
+echo ${modules}
+
+for target in ${YARN_PROJECTS}
+do
+  cd ${CUR}/${target}
+  yarn install
+  yarn add --dev ${dev_modules}
+  yarn add ${modules}
+  yarn upgrade
+done
+
+# To solve the problem of not being able to delete the node_modules directory
+rm -rf node_modules
+
+cd ${CUR}

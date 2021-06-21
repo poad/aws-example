c83c9322c4352770689e8daacc6729a7e304c3c0
Kenji Saito <ken-yo@mbr.nifty.com>
2021-06-22 08:41:05 +0900

    Remove serverless framework (#168)
    * R.I.P. Serverless framework
    
    * Fix the docker build
    
    * Add the test and Hadlint for lambda container

---
+++ lambda/container/hello-rust-lambda/lib/hello-rust-lambda-stack.ts
@@ -0,0 +1,40 @@
+import * as cdk from '@aws-cdk/core';
+import { DockerImageFunction, DockerImageCode } from '@aws-cdk/aws-lambda';
+import { HttpApi, HttpMethod } from '@aws-cdk/aws-apigatewayv2';
+import { LambdaProxyIntegration } from '@aws-cdk/aws-apigatewayv2-integrations';
+import { RetentionDays } from '@aws-cdk/aws-logs';
+
+export interface HelloRustLambdaStackProps extends cdk.StackProps {
+  name: string,
+  region: string,
+  environment: string,
+}
+
+export class HelloRustLambdaStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props: HelloRustLambdaStackProps) {
+    super(scope, id, props);
+
+    const fn = new DockerImageFunction(this, 'hello-rust-lambda-function', {
+      code: DockerImageCode.fromImageAsset('lambda', {
+        target: 'release'
+      }),
+      functionName: props.name,
+      logRetention: RetentionDays.ONE_DAY,
+      retryAttempts: 0,
+    });
+
+    const api = new HttpApi(this, "HttpApi", {
+      apiName: 'Hello Rust Lambda API',
+      defaultIntegration: new LambdaProxyIntegration({
+        handler: fn
+      })
+    });
+    api.addRoutes({
+      path: "/{proxy+}",
+      methods: [HttpMethod.ANY],
+      integration: new LambdaProxyIntegration({
+        handler: fn
+      }),
+    });
+  }
+}
---
+++ lambda/container/hello-rust-lambda/test/hello-rust-lambda.test.ts
@@ -0,0 +1,20 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as HelloRustLambda from '../lib/hello-rust-lambda-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new HelloRustLambda.HelloRustLambdaStack(app, 'MyTestStack', {
+      name: 'test-hello-rust-lambda',
+      region: 'us-west-2',
+      environment: 'test',
+      env: {
+        region: 'us-west-2',
+      },
+    });
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});

b2dae6acbda62028deac6753433f422015596d32
Kenji Saito <ken-yo@mbr.nifty.com>
2021-11-07 12:59:49 +0900

    React Hook 化

--- cognito/cognito-saml-next-js/webapp/src/pages/index.tsx
+++ cognito/cognito-saml-next-js/webapp/src/pages/index.tsx
@@ -1,63 +1,32 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import { Box } from '@mui/system';
-import Amplify, { Auth } from 'aws-amplify';
+import Amplify from 'aws-amplify';
 import { AmplifySignOut } from '@aws-amplify/ui-react';
 import awsconfig from '../aws-exports';
-import qs from 'qs';
 import AuthButton from 'components/AuthButton';
-import { CognitoUserInterface } from '@aws-amplify/ui-components';
+import useAuth from 'components/hooks/useAuth';
 
 Amplify.configure(awsconfig);
 
 const Home = (): JSX.Element => {
 
-  const [user, setUser] = React.useState<CognitoUserInterface | undefined>();
-  const [token, setToken] = React.useState<string | undefined>(undefined);
-
-  const authHandle = async () => {
-    if (window !== undefined) {
-      const hash = window.location.hash.replace(/^#?\/?/, '');
-      const idToken = qs.parse(hash).id_token?.toString();
-      const expiresIn = Number(qs.parse(hash).expires_in?.toString());
-
-      if (idToken !== undefined && idToken.length === 0) {
-        Auth.federatedSignIn('AzureAD', {
-          token: idToken,
-          expires_at: expiresIn,
-        }, {
-          name: '',
-        });
-      }
-    }
-    
-    Auth.currentSession()
-      .then(session => setToken(session.getIdToken().getJwtToken()));
-
-    Auth.currentAuthenticatedUser()
-      .then(currentUser => {
-        setUser(currentUser);
-      });
-  };
-
-  React.useEffect(() => {
-    Promise.all([authHandle()]);
-  }, []);
+  const auth = useAuth(typeof window !== 'undefined' ? window.location.hash : undefined);
 
   return (
     <Layout>
-      {token !== undefined
-        ? (user !== undefined
+      {auth?.token !== undefined
+        ? (auth?.user !== undefined
           ? (
             <>
               <Box sx={{ width: '20rem', mt: '3rem' }}>
                 <AmplifySignOut />
               </Box>
               <Box sx={{ color: '#2d2d2d', whiteSpace: 'pre-wrap' }}>
-                {user.attributes.name}
+                {auth.user.attributes.name}
               </Box>
               <Box sx={{ color: '#2d2d2d', whiteSpace: 'pre-wrap' }}>
-                {user.attributes.email}
+                {auth.user.attributes.email}
               </Box>
             </>
           )

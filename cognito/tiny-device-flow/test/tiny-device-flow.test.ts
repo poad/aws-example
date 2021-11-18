f71e607836a4c13b8619c7307069d852bd6b0c1e
Kenji Saito <ken-yo@mbr.nifty.com>
2021-11-19 01:01:06 +0900

    Fix the yarn test error

--- cognito/tiny-device-flow/test/tiny-device-flow.test.ts
+++ cognito/tiny-device-flow/test/tiny-device-flow.test.ts
@@ -3,18 +3,24 @@ import * as cdk from '@aws-cdk/core';
 import * as TinyDeviceFlow from '../lib/tiny-device-flow-stack';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new TinyDeviceFlow.TinyDeviceFlowStack(app, 'MyTestStack', {
-      name: 'test',
-      userPool: 'test',
-      region: 'test',
-      environment: 'test',
-      domain: 'test',
-    
-    });
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+  const app = new cdk.App();
+  // WHEN
+  const stack = new TinyDeviceFlow.TinyDeviceFlowStack(app, 'MyTestStack', {
+    name: 'test',
+    userPool: 'test',
+    region: 'test',
+    environment: 'test',
+    domain: 'test',
+    scopes: {
+      phone: false,
+      email: false,
+      openid: false,
+      profile: false,
+      'aws.cognito.signin.user.admin': false,
+    },
+  });
+  // THEN
+  expectCDK(stack).to(matchTemplate({
+    "Resources": {}
+  }, MatchStyle.EXACT))
 });

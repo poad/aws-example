a3bd60f29318f7feef57ceafa915658909e398dc
Kenji Saito <ken-yo@mbr.nifty.com>
2022-04-17 16:41:05 +0900

    Add the next.js app hosted by amplify

---
+++ amplify/amplify_nextapp/pages/_app.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import type { AppProps } from 'next/app';
+
+import '../styles/globals.css';
+
+function MyApp({ Component }: AppProps) {
+  return <Component />;
+}
+
+export default MyApp;

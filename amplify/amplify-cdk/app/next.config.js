a6943a47eea9f9f9b5e557171958cd76f012e75d
Kenji Saito <ken-yo@mbr.nifty.com>
2022-07-02 12:57:07 +0900

    Fix deploy error by amplify CD

--- amplify/amplify-cdk/app/next.config.js
+++ amplify/amplify-cdk/app/next.config.js
@@ -1,23 +1,18 @@
 /** @type {import('next').NextConfig} */
-import withPlugins from 'next-compose-plugins';
-import withPreact from 'next-plugin-preact';
-import withBundleAnalyzer from '@next/bundle-analyzer';
+const withBundleAnalyzer = require('@next/bundle-analyzer');
 
 const analyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
-const config = withPlugins([
-  [analyzer, withPreact],
-],
-  {
-    webpack5: true,
-    reactStrictMode: true,
-    esmExternals: true,
-    swcLoader: true,
-    swcMinify: true,
-    experimental: {
-      modern: true,
-    },
-  });
+const config = analyzer({
+  webpack5: true,
+  reactStrictMode: true,
+  esmExternals: true,
+  swcLoader: true,
+  swcMinify: true,
+  experimental: {
+    modern: true,
+  },
+});
 
-export default config;
+module.exports = config;

ddfd15efcb1faae98f6aa5c4a92880c858be7825
Kenji Saito <ken-yo@mbr.nifty.com>
2022-09-13 03:52:48 +0900

    Bumps node modules

--- lambda/apollo-server-url/bin/apollo-server.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { ApolloServerStack } from '../lib/apollo-server-stack';
-
-const app = new cdk.App();
-new ApolloServerStack(app, 'apollo-server-stack', {
-});

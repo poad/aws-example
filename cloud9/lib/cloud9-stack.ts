759713700cfb3b9421c5127c991bdcb3d0aca23f
Kenji Saito <ken-yo@mbr.nifty.com>
2022-09-26 09:12:47 +0900

    Add the auto sleep for Cloud9

--- cloud9/lib/cloud9-stack.ts
+++ cloud9/lib/cloud9-stack.ts
@@ -171,6 +171,65 @@ EOS`,
       'sudo -u ubuntu /home/ubuntu/.volta/bin/volta install node@lts && npm -g install yarn',
       'echo \'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"\' >> /home/ubuntu/.bash_profile',
       'sudo -u ubuntu NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"',
+      'echo "SHUTDOWN_TIME=30" > /home/ubuntu/.c9/autoshutdown-configuration',
+      'chown ubuntu:ubuntu /home/ubuntu/.c9/autoshutdown-configuration',
+      `cat << 'EOS' >> /home/ubuntu/.c9/stop-if-inactive.sh
+#!/bin/bash
+set -euo pipefail
+CONFIG=$(cat /home/ubuntu/.c9/autoshutdown-configuration)
+SHUTDOWN_TIMEOUT=\${CONFIG#*=}
+if ! [[ $SHUTDOWN_TIMEOUT =~ ^[0-9]*$ ]]; then
+    echo "shutdown timeout is invalid"
+    exit 1
+fi
+is_shutting_down() {
+    is_shutting_down_ubuntu &> /dev/null || is_shutting_down_al1 &> /dev/null || is_shutting_down_al2 &> /dev/null
+}
+is_shutting_down_ubuntu() {
+    local TIMEOUT
+    TIMEOUT=$(busctl get-property org.freedesktop.login1 /org/freedesktop/login1 org.freedesktop.login1.Manager ScheduledShutdown)
+    if [ "$?" -ne "0" ]; then
+        return 1
+    fi
+    if [ "$(echo $TIMEOUT | awk "{print \\$3}")" == "0" ]; then
+        return 1
+    else
+        return 0
+    fi
+}
+is_shutting_down_al1() {
+    pgrep shutdown
+}
+is_shutting_down_al2() {
+    local FILE
+    FILE=/run/systemd/shutdown/scheduled
+    if [[ -f "$FILE" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+is_vfs_connected() {
+    pgrep -f vfs-worker >/dev/null
+}
+
+if is_shutting_down; then
+    if [[ ! $SHUTDOWN_TIMEOUT =~ ^[0-9]+$ ]] || is_vfs_connected; then
+        sudo shutdown -c
+    fi
+else
+    if [[ $SHUTDOWN_TIMEOUT =~ ^[0-9]+$ ]] && ! is_vfs_connected; then
+        sudo shutdown -h $SHUTDOWN_TIMEOUT
+    fi
+fi > "$VFS_CHECK_FILE_PATH"
+
+chmod +x "$VFS_CHECK_FILE_PATH"
+
+echo "* * * * * root $VFS_CHECK_FILE_PATH" > /etc/cron.d/c9-automatic-shutdown
+
+EOS`,
+      'chown ubuntu:ubuntu /home/ubuntu/.c9/stop-if-inactive.sh',
+      'chmod 744 /home/ubuntu/.c9/stop-if-inactive.sh',
     );
 
     const securityGroup = new SecurityGroup(this, 'SecurityGroup', {

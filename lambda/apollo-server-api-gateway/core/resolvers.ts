9fc95f049dfb8909f36a696b61977435847c5f2c
Kenji Saito <ken-yo@mbr.nifty.com>
2022-10-06 21:15:23 +0900

    Applo Server のサンプルを修正

--- lambda/apollo-server-api-gateway/core/resolvers.ts
+++ lambda/apollo-server-api-gateway/core/resolvers.ts
@@ -1,13 +1,80 @@
-const resolvers = {
-  Query: {
-    hello: (_: any, args: { [key: string]: string | number }) => (args.message ? args.message.toString() : 'Hello, World!'),
-    dump: (_: any, args: { [key: string]: string | number }, context: any) => {
-      const message = args.message?.toString();
-      const headers = JSON.stringify(context.headers);
-      return { message, headers };
-    },
+import {
+  Author, AuthorResolvers, Post, PostResolvers, QueryAuthorArgs, QueryPostArgs, QueryResolvers, Resolvers,
+} from './generated/graphql';
 
+// example data
+const authors: Author[] = [
+  { id: 1, firstName: 'Tom', lastName: 'Coleman' },
+  { id: 2, firstName: 'Sashko', lastName: 'Stubailo' },
+  { id: 3, firstName: 'Mikhail', lastName: 'Novikov' },
+];
+
+const posts = [
+  {
+    id: 1, authorId: 1, title: 'Introduction to GraphQL', votes: 2,
   },
+  {
+    id: 2, authorId: 2, title: 'Welcome to Meteor', votes: 3,
+  },
+  {
+    id: 3, authorId: 2, title: 'Advanced GraphQL', votes: 1,
+  },
+  {
+    id: 4, authorId: 3, title: 'Launchpad is Cool', votes: 7,
+  },
+];
+
+const resolvers: Resolvers = {
+  Query: {
+    posts: () => {
+      console.log('Query.posts');
+      return posts.map((it) => ({
+        ...it,
+        author: authors.find((author) => author.id === it.authorId),
+      }));
+    },
+    post: (_, { id }: QueryPostArgs) => {
+      console.log('Query.post');
+      const item = posts.find((it) => it.id === id);
+      if (!item) {
+        return null;
+      }
+      const author = authors.find((it) => it.id === item.authorId);
+      return {
+        ...item,
+        author,
+      } || null;
+    },
+    author: (_, { id }: QueryAuthorArgs): Author | null => {
+      console.log('Query.author');
+      return authors.find((it) => it.id === id) || null;
+    },
+  } as QueryResolvers,
+
+  Mutation: {
+    upvotePost(_, { postId }) {
+      const post = posts.find((it) => it.id === postId);
+      if (!post) {
+        throw new Error(`Couldn't find post with id ${postId}`);
+      }
+      post.votes += 1;
+      return post;
+    },
+  },
+
+  Author: {
+    posts: (author: Author): Post[] | null => {
+      console.log('Auther.posts');
+      return posts.filter((it) => it.authorId === author.id) || null;
+    },
+  } as AuthorResolvers,
+
+  Post: {
+    author: (post: Post): Author | null => {
+      console.log('Post.author');
+      return post.author ? authors.find((it) => it.id === post.author?.id) || null : null;
+    },
+  } as PostResolvers,
 };
 
 export default resolvers;

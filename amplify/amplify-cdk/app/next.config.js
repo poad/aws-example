d82cf95ecd6395e12f98a296fa595f990c739c21
Kenji Saito <ken-yo@mbr.nifty.com>
2022-10-26 19:47:12 +0900

    Bumps node modules

--- amplify/amplify-cdk/app/next.config.js
+++ amplify/amplify-cdk/app/next.config.js
@@ -1,15 +1,14 @@
-/** @type {import('next').NextConfig} */
 const withBundleAnalyzer = require('@next/bundle-analyzer');
-const withPreact = require('next-plugin-preact');
 
+/** @type {import('next').NextConfig} */
 const config = withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
-})(withPreact({
+})({
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
     esmExternals: true,
   },
-}));
+});
 
 module.exports = config;

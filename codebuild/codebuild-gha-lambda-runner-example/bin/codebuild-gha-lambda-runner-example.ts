3b6aa9c4a4ba7c955a63abf1011c47edf2703552
Kenji Saito <ken-yo@mbr.nifty.com>
2024-05-08 22:50:32 +0900

    Add the example for ecr custom image

--- codebuild/codebuild-gha-lambda-runner-example/bin/codebuild-gha-lambda-runner-example.ts
+++ codebuild/codebuild-gha-lambda-runner-example/bin/codebuild-gha-lambda-runner-example.ts
@@ -1,11 +1,32 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
+import * as codebuild from "aws-cdk-lib/aws-codebuild";
 import { CodebuildGhaLambdaRunnerExampleStack } from "../lib/codebuild-gha-lambda-runner-example-stack";
+
+const projects = [
+  {
+    projectName: "codebuild-gha-lambda-runner-example",
+    owner: "poad",
+    repo: "aws-codebuild-github-lambda-runner-node-example",
+    buildImage: codebuild.LinuxArmLambdaBuildImage.AMAZON_LINUX_2023_NODE_20,
+    prefix: '',
+  },
+  {
+    projectName: "codebuild-gha-lambda-runner-example-rust-dev1",
+    owner: "poad",
+    repo: "aws-codebuild-github-lambda-runner-rust-example",
+    buildImage: codebuild.LinuxLambdaBuildImage.AMAZON_LINUX_2023_NODE_20,
+    prefix: 'Rust',
+    customImage: 'public.ecr.aws/docker/library/rust:latest',
+  },
+];
 
 const app = new cdk.App();
 new CodebuildGhaLambdaRunnerExampleStack(
   app,
   "codebuild-gha-lambda-runner-example-stack",
-  {},
+  {
+    projects,
+  },
 );
--- codebuild/codebuild-gha-lambda-runner-example/lib/codebuild-gha-lambda-runner-example-stack.ts
+++ codebuild/codebuild-gha-lambda-runner-example/lib/codebuild-gha-lambda-runner-example-stack.ts
@@ -1,32 +1,128 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as codebuild from "aws-cdk-lib/aws-codebuild";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as awslogs from "aws-cdk-lib/aws-logs";
+
+interface CodebuildGhaLambdaRunnerExampleStackProps extends cdk.StackProps {
+  projects: {
+    projectName: string,
+    owner: string,
+    repo: string,
+    buildImage: cdk.aws_codebuild.IBuildImage,
+    prefix: string,
+    customImage?: string,
+  }[]
+}
 
 export class CodebuildGhaLambdaRunnerExampleStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: CodebuildGhaLambdaRunnerExampleStackProps) {
     super(scope, id, props);
 
-    const project = new codebuild.Project(this, "CodeBuildProject", {
-      projectName: "codebuild-gha-lambda-runner-example",
-      source: codebuild.Source.gitHub({
-        owner: "poad",
-        repo: "aws-codebuild-github-lambda-runner-node-example",
-        webhook: true,
-        webhookFilters: [
-          codebuild.FilterGroup.inEventOf(codebuild.EventAction.PUSH),
-        ],
-      }),
-      environment: {
-        buildImage:
-          codebuild.LinuxArmLambdaBuildImage.AMAZON_LINUX_2023_NODE_20,
-        computeType: codebuild.ComputeType.LAMBDA_1GB,
-      },
-    });
+    props.projects.forEach(({
+      projectName,
+      owner,
+      repo,
+      buildImage,
+      prefix,
+      customImage,
+    }) => {
+      const logs = new awslogs.LogGroup(this, `${prefix}LogGroup`, {
+        logGroupName: `/aws/codebuild/${projectName}`,
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        retention: awslogs.RetentionDays.ONE_DAY,
+      });
 
-    const cfnProject = project.node.defaultChild as codebuild.CfnProject;
-    cfnProject.addOverride(
-      "Properties.Triggers.FilterGroups.0.0.Pattern",
-      "WORKFLOW_JOB_QUEUED",
-    );
+      const role = new iam.Role(this, `${prefix}ProjectRole`, {
+        roleName: `${projectName}-service-role`,
+        assumedBy: new iam.ServicePrincipal('codebuild.amazonaws.com'),
+        inlinePolicies: {
+          'logs-policy': new iam.PolicyDocument({
+            statements: [
+              new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: [
+                  'logs:CreateLogStream',
+                  'logs:PutLogEvents',
+                ],
+                resources: [
+                  `${logs.logGroupArn}:*`
+                ],
+              }),
+            ],
+          }),
+          's3-policy': new iam.PolicyDocument({
+            statements: [
+              new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: [
+                  "s3:PutObject",
+                  "s3:GetObject",
+                  "s3:GetObjectVersion",
+                  "s3:GetBucketAcl",
+                  "s3:GetBucketLocation"
+                ],
+                resources: [
+                  `arn:aws:s3:::codepipeline-${this.region}:*`
+                ],
+              }),
+            ],
+          }),
+          'codebuild-policy': new iam.PolicyDocument({
+            statements: [
+              new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: [
+                  "codebuild:CreateReportGroup",
+                  "codebuild:CreateReport",
+                  "codebuild:UpdateReport",
+                  "codebuild:BatchPutTestCases",
+                  "codebuild:BatchPutCodeCoverages"
+                ],
+                resources: [
+                  `arn:aws:codebuild:${this.region}:${this.account}:report-group/${projectName}-*`
+                ],
+              }),
+            ],
+          }),
+
+        },
+
+      });
+
+      const project = new codebuild.Project(this, `${prefix}CodeBuildProject`, {
+        projectName,
+        source: codebuild.Source.gitHub({
+          owner,
+          repo,
+          webhook: true,
+          webhookFilters: [
+            codebuild.FilterGroup.inEventOf(codebuild.EventAction.PUSH),
+          ],
+        }),
+        environment: {
+          buildImage,
+          computeType: codebuild.ComputeType.LAMBDA_1GB,
+        },
+        role,
+      });
+
+      const cfnProject = project.node.defaultChild as codebuild.CfnProject;
+      cfnProject.addOverride(
+        "Properties.Triggers.FilterGroups.0.0.Pattern",
+        "WORKFLOW_JOB_QUEUED",
+      );
+
+      if (customImage) {
+        cfnProject.addOverride(
+          "Properties.Environment.Image",
+          customImage,
+        );
+        cfnProject.addPropertyOverride(
+          'Environment.ImagePullCredentialsType',
+          'SERVICE_ROLE'
+        );
+      }
+    });
   }
 }

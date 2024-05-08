461d1fc795b6af9407aa83a33dceaa013c9fbc9b
Kenji Saito <ken-yo@mbr.nifty.com>
2024-05-08 23:27:02 +0900

    Fix the test

--- codebuild/codebuild-gha-lambda-runner-example/test/codebuild-gha-lambda-runner-example.test.ts
+++ codebuild/codebuild-gha-lambda-runner-example/test/codebuild-gha-lambda-runner-example.test.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import * as CodebuildGhaLambdaRunnerExample from '../lib/codebuild-gha-lambda-runner-example-stack';
+import * as codebuild from "aws-cdk-lib/aws-codebuild";
 import { test } from 'vitest';
 
 // example test. To run these tests, uncomment this file along with the
@@ -8,7 +9,18 @@ import { test } from 'vitest';
 test('SQS Queue Created', () => {
   const app = new cdk.App();
     // WHEN
-  const stack = new CodebuildGhaLambdaRunnerExample.CodebuildGhaLambdaRunnerExampleStack(app, 'MyTestStack');
+  const stack = new CodebuildGhaLambdaRunnerExample.CodebuildGhaLambdaRunnerExampleStack(app, 'MyTestStack', {
+    projects: [
+      {
+        projectName: "codebuild-gha-lambda-runner-example-rust-dev1",
+        owner: "poad",
+        repo: "aws-codebuild-github-lambda-runner-rust-example",
+        buildImage: codebuild.LinuxLambdaBuildImage.AMAZON_LINUX_2023_NODE_20,
+        prefix: 'Rust',
+        customImage: 'public.ecr.aws/docker/library/rust:latest',
+      }
+    ]
+  });
     // THEN
   const template = Template.fromStack(stack);
 

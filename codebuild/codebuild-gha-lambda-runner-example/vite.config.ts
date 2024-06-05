4915ec1af8cd85bbff8f8db1302064ec13c40cbd
Kenji Saito <ken-yo@mbr.nifty.com>
2024-06-05 12:21:04 +0900

    Bumps node modules

--- codebuild/codebuild-gha-lambda-runner-example/vite.config.ts
+++ codebuild/codebuild-gha-lambda-runner-example/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   ],
   test: {
     coverage: {
-      reporter: ['json', 'html', 'cobertura']
+      reporter: ['json', 'json-summary', 'html', 'cobertura']
     }
   },
 });

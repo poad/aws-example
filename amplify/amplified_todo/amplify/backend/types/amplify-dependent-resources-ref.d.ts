14762a0d8a2feacab46b7325e9feac70a68c1fed
Kenji Saito <ken-yo@mbr.nifty.com>
2024-07-31 12:53:07 +0900

    Bumps node modules

--- amplify/amplified_todo/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "amplify": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}

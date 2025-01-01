c269f9f8155825dc865643e29cb484a4a66f0dca
Kenji Saito <ken-yo@mbr.nifty.com>
2025-01-01 18:13:55 +0900

    Bumps node modules

--- amplify/amplify_nextapp/next.config.js
+++ amplify/amplify_nextapp/next.config.js
@@ -4,7 +4,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer');
 const nextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
-  swcMinify: true,
   experimental: {
     esmExternals: true,
   },

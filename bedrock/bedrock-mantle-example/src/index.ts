3359a1cedeb9bc3cb273a7dec4daf40b18e90382
Kenji Saito <ken-yo@mbr.nifty.com>
2026-07-15 05:12:06 +0900

    Bumps node modules

--- bedrock/bedrock-mantle-example/src/index.ts
+++ bedrock/bedrock-mantle-example/src/index.ts
@@ -1,5 +1,5 @@
-import OpenAI from "openai";
-import { getTokenProvider } from "@aws/bedrock-token-generator";
+import OpenAI from 'openai';
+import { getTokenProvider } from '@aws/bedrock-token-generator';
 import { config } from '@dotenvx/dotenvx';
 
 config();
@@ -8,13 +8,13 @@ const provideToken = getTokenProvider();
 
 const client = new OpenAI({
   apiKey: await provideToken(),
-  baseURL: "https://bedrock-mantle.us-east-1.api.aws/v1",
-  project: "default",
+  baseURL: 'https://bedrock-mantle.us-east-1.api.aws/v1',
+  project: 'default',
 });
 
 const response = await client.responses.create({
-  model: "openai.gpt-oss-20b",
+  model: 'openai.gpt-oss-20b',
   // model: "openai.gpt-5.4",
-  input: [{ role: "user", content: "What is Amazon Bedrock?" }],
+  input: [{ role: 'user', content: 'What is Amazon Bedrock?' }],
 });
 console.log(response.output_text);
